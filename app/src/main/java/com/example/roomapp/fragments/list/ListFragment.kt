af7f89c1b146b4891673dad70007b900f4307168
Jagi <ba87688@gmail.com>
2022-06-13 13:52:14 -0500

    viewbinding addition and fixing navigation

--- app/src/main/java/com/example/roomapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/roomapp/fragments/list/ListFragment.kt
@@ -7,19 +7,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.roomapp.R
+import com.example.roomapp.databinding.FragmentListBinding
 import kotlinx.android.synthetic.main.fragment_list.view.*
 
 class ListFragment : Fragment() {
+
+    private var _binding: FragmentListBinding?=null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater,container,false)
+        val view = binding.root
+
 
         view.floatingActionButton.setOnClickListener {
-            findNavController().navigate(R.id.action_listFragment_to_addingFragment)
+            findNavController().navigate(R.id.action_listFragment_to_addingFragment2)
         }
         return view
     }
