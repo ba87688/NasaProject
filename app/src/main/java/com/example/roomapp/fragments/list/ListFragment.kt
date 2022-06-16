178b6805cc9903b99d08cdfeb907d9dedc0cdc8a
Jagi <ba87688@gmail.com>
2022-06-16 03:50:45 -0500

    Delete all option

--- app/src/main/java/com/example/roomapp/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/roomapp/fragments/list/ListFragment.kt
@@ -2,9 +2,6 @@ package com.example.databasetesting.fragments.list
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -15,7 +12,10 @@ import com.example.roomapp.data.UserViewModel
 import com.example.roomapp.databinding.FragmentListBinding
 import kotlinx.android.synthetic.main.fragment_list.view.*
 import android.app.Activity
+import android.app.AlertDialog
+import android.view.*
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 
@@ -36,7 +36,7 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentListBinding.inflate(inflater,container,false)
         val view = binding.root
-
+setHasOptionsMenu(true)
         val adapter = ListViewAdapter()
         //recyclerview
         val rv = view.recyclerView
@@ -56,5 +56,31 @@ class ListFragment : Fragment() {
         }
         return view
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.menu_delete){
+            deleteAllUsers()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    private fun deleteAllUsers(){
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setPositiveButton("Yes"){_,_->
+            userViewModel.deleteAllUsers()
+            Toast.makeText(requireContext(),"You deleted all users",Toast.LENGTH_SHORT).show()
+
+        }
+        builder.setNegativeButton("No"){
+            _,_->
+        }
+        builder.setTitle("delete all?")
+        builder.setMessage("are you sure?")
+        builder.create().show()
+    }
+
 
 }
